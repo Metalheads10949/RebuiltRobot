34f8843325983ecc5c991857377490f7ab7b10c5
declanaptowicz <declan.aptowicz@gmail.com>
2026-03-02 15:16:04 -0500

    contants

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+public class Constants {
+    public static final class IndexerConstants {
+
+    }
+    public static final class DrivetrainConstants {
+        //we might have to put ports in here but IDK
+    }
+    public static final class IntakeConstants {
+
+    }
+    public static final class LauncherConstants {
+
+    }
+}
